7c055f2d64d6975a9c5f3ae3277f77b26ad3f4bf
suyu <suyu0925@gmail.com>
2024-12-18 14:49:57 +0800

    feat: add image request interception to debug script

--- scripts/debugWithArtifact.ts
+++ scripts/debugWithArtifact.ts
@@ -1,9 +1,27 @@
 import fs from 'fs'
 import puppeteer from 'puppeteer'
+import type { Browser, Page } from 'puppeteer'
+
+const ignoreImages = async (page: Page) => {
+  await page.setRequestInterception(true)
+
+  page.on('request', (request) => {
+    if (request.isInterceptResolutionHandled()) {
+      return
+    }
+    const resourceType = request.resourceType()
+    if (resourceType === 'image') {
+      request.abort()
+    } else {
+      request.continue()
+    }
+  })
+}
 
 const browser = await puppeteer.launch()
 const page = await browser.newPage()
 await page.setViewport({ width: 1080, height: 1024 })
+await ignoreImages(page)
 
 const debugPath = 'debug-artifact'
 fs.mkdirSync(debugPath, { recursive: true })
