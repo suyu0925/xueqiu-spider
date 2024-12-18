aaae998868ac603bd65b48bf76d611e9163e2151
suyu <suyu0925@gmail.com>
2024-12-18 15:08:08 +0800

    refactor: remove account iteration from debug script and streamline screenshot capture

--- scripts/debugWithArtifact.ts
+++ scripts/debugWithArtifact.ts
@@ -1,5 +1,4 @@
 import fs from 'fs'
-import accounts from '../dev-accounts.json' with { type: 'json' }
 import XueqiuApi from './api'
 
 const debugPath = 'debug-artifact'
@@ -8,7 +7,4 @@ fs.mkdirSync(debugPath, { recursive: true })
 const api = new XueqiuApi()
 await api.init()
 await api.screenshot(`${debugPath}/home.png`)
-for (const account of accounts) {
-  console.log(`Fetching timeline for ${account.screen_name}`)
-}
 await api.dispose()
