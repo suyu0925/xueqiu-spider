a2e5d93a9eebcfe1304ee93ecb81e2bbfba0c483
suyu <suyu0925@gmail.com>
2024-12-18 15:04:15 +0800

    feat: enhance debug script to iterate over accounts and capture multiple screenshots

--- scripts/debugWithArtifact.ts
+++ scripts/debugWithArtifact.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import accounts from "../dev-accounts.json" with { type: "json" }
 import XueqiuApi from './api'
 
 const debugPath = 'debug-artifact'
@@ -6,5 +7,9 @@ fs.mkdirSync(debugPath, { recursive: true })
 
 const api = new XueqiuApi()
 await api.init()
-await api.screenshot(`${debugPath}/home.png`)
+for (const account of accounts) {
+  await api.screenshot(`${debugPath}/home.png`)
+  const timelineRes = await api.fetchUserTimeline(account.id)
+  await api.screenshot(`${debugPath}/home2.png`)
+}
 await api.dispose()
